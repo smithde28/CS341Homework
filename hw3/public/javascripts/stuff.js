e5e2590ae9c7f255b6278324ea3a8d7b62c2ba06
Derric Smith <hbhsdxs@gmail.com>
2026-02-04 19:22:01 -0800

    Fixed index.html, moved js code to separate file

---
+++ hw3/public/javascripts/stuff.js
@@ -0,0 +1,54 @@
+eventHandler = submission(event)
+{
+  /* do stuff */
+}
+
+/* Function that alerts you if "vegan" is included, says your order is placed if not. */
+$function submission()
+{
+  const notes = document.getElementById("notes").value.trim().toLowerCase();
+
+  if (notes.includes("vegan"))
+  {
+    alert("Warning: These cheesecakes contain dairy, proceed with caution!");
+  }
+  else
+  {
+    /* Gets the id's of each element */
+    const element1 = document.getElementById("notes"); 
+    const element2 = document.getElementById("orderForm"); 
+    const element3 = document.getElementById("amount"); 
+    const element4 = document.getElementById("orderButton"); 
+    
+    /* Removes elements */
+    element1.remove(); 
+    element2.remove();  
+    element3.remove(); 
+    element4.remove();
+
+    alert("Your order has been placed!");
+  }
+
+  return false;
+}
+
+/* Gets id's of month menu items */
+const currentMonth = document.getElementById("currentMonth");
+const menu = document.getElementById("monthMenu");
+const months = document.getElementsByClassName("month");
+
+/* Open menu */
+currentMonth.onclick = function ()
+{
+  menu.style.display = "block";
+};
+
+/* Select month */
+for (let i = 0; i < months.length; i++)
+{
+  months[i].onclick = function ()
+  {
+    currentMonth.innerHTML = this.innerHTML;
+    menu.style.display = "none";
+  };
+}
